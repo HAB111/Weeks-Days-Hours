aed95c945ade854cbb88440bf6c111168c76982f
Hamad Bangash <hbangash@xpanxion.com>
2022-07-11 18:16:42 -0400

    MaxOfEachLine

---
+++ src/MaxOfEachLine/MaximumOfEachLine.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MaximumOfEachLine
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File f1 = new File("C:\\Users\\HamadBangash\\IdeaProjects\\Weeks Days Hours\\src\\data.txt");
+        Scanner scan = new Scanner(f1);
+    
+    }
+
+}
